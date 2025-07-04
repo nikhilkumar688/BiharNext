066832fac4571715fd453f54a84adc83c07094a1
Nikhil Kumar <your_email@example.com>
2025-07-04 06:57:49 +0530

    installing shadcnui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-
+import React from 'react'
+import { Button } from './components/ui/button'
 const App = () => {
-  return (
+  return (<div>
     <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
     </h1>
+    <Button className="bg-red-500" variant="ghost">Click me</Button>
+    </div>
   )
 }
 
